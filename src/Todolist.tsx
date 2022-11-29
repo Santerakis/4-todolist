5d787e71e51fa3bd5d078ff4261b19d26d30d366
Sergey <santerakis@gmail.com>
2022-11-29 19:16:59 +0200

    added error

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -19,11 +19,14 @@ type PropsType = {
 export function Todolist(props: PropsType) {
 
     let [title, setTitle] = useState("")
+    let [error, setError] = useState<string | null>(null)
 
     const addTask = () => {
         if (title.trim() !== '') {
             props.addTask(title.trim());
             setTitle("");
+        } else {
+            setError('Title is required')
         }
     }
 
@@ -48,8 +51,10 @@ export function Todolist(props: PropsType) {
             <input value={title}
                    onChange={onChangeHandler}
                    onKeyDown={onKeyPressHandler}
+                   className={error ?'error' :''}
             />
             <button onClick={addTask}>+</button>
+            {error && <div className={'error-message'}>{error}</div>}
         </div>
         <ul>
             {
